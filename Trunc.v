4c10567b9be2e5d716b89e1302b8bf5b8a9edf9a
TheoWinterhalter <isheeft@gmail.com>
2016-04-20 11:22:41 +0200

    With the truncation, up to Trunc A inhabited

--- Trunc.v
+++ Trunc.v
@@ -1,8 +1,5 @@
 Set Universe Polymorphism.
 
-Inductive Trunc A : Prop :=
-  tr : A -> Trunc A.
-
 Definition ishProp A := forall x y : A, x = y.
 
 Definition comp {A B C} (f : B -> C) (g : A -> B) := fun x => f (g x).
@@ -14,7 +11,7 @@ Notation "f ~ g" := (homo f g) (at level 87).
 Definition id A := fun x : A => x.
 
 Definition isEquiv {A B} (f : A -> B) :=
-  { g : B -> A & (f ∘ g ~ id B) /\ (g ∘ f ~ id A)}.
+  { g : B -> A & (f ∘ g ~ id B) /\ (g ∘ f ~ id A) }.
 
 Definition Equiv A B :=
   { f : A -> B & isEquiv f }.
@@ -43,6 +40,35 @@ Section EM.
   Qed.
 
 End EM.
+
+(* With the truncation *)
+
+Inductive Trunc A : Prop :=
+  tr : A -> Trunc A.
+Arguments tr {A} a.
+
+Definition PropEquiv A B : Prop :=
+  exists f : A -> B, exists g : B -> A, (f ∘ g ~ id B) /\ (g ∘ f ~ id A).
+
+Section Trunc.
+
+  Variable A : Type.
+  Variable h : ishProp A.
+  Variable th : ishProp (Trunc A).
+
+  Lemma equiv_trunc : PropEquiv A (Trunc A).
+  Proof.
+    apply (Trunc_ind A).
+    - intro a.
+      exists (fun a : A => tr a). exists (fun aa => a). split.
+      + unfold homo, comp, id. intro aa.
+        apply th.
+      + unfold comp, homo, id. intro a'.
+        apply h.
+    -
+  Abort.
+
+End Trunc.
 
 
 
