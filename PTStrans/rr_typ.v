75d93bd28e45234adcc97785ddb5743f0fda8e56
Théo Winterhalter <isheeft@gmail.com>
2016-06-18 18:21:08 +0200

    Starting to write the transformation...
    Several problems.
    - I can’t get to write the theorem I want.
    - I need to have information about the type when translating.

--- PTStrans/rr_typ.v
+++ PTStrans/rr_typ.v
@@ -1,15 +1,21 @@
 (** *Typing rules for PTSF.*)
-Require Import f_term f_env rr_term rr_env.
+Require Import f_term f_env f_typ f_typ2 rr_term rr_env.
+Require Import ut_term ut_red ut_env ut_typ ut_sr ut_typ_eq.
 Require Import base.
 Require Import List.
 Require Import Peano_dec.
 Require Import Compare_dec.
 Require Import Lt Le Gt Plus Minus.
 
-Module f_typ_mod (X : term_sig) (Y : pts_sig X) (FTM : f_term_mod X) (FEM : f_env_mod X FTM)
+Module f_typ_mod (X : term_sig) (Y : pts_sig X) (FTM : f_term_mod X)
+                 (FEM : f_env_mod X FTM)
+                 (UTM : ut_term_mod X) (UEM : ut_env_mod X UTM)
+                 (URM : ut_red_mod X UTM) (PTS : ut_sr_mod X Y UTM UEM URM)
                  (RE : resizing_env X Y FTM FEM) (TM : rr_term_mod X Y FTM FEM RE)
                  (EM : rr_env_mod X Y FTM FEM RE TM).
   Import X Y FTM FEM TM EM.
+  (* Include (f_typ_mod X Y FTM FEM). *)
+  (* Include (f_typ2_mod  X Y FTM FEM UTM URM UEM PTS). *)
 
   Reserved Notation "Γ ⊢ t : T" (at level 80, t, T at level 30, no associativity) .
   Reserved Notation "Γ ⊣ " (at level 80, no associativity).
@@ -100,11 +106,11 @@ Module f_typ_mod (X : term_sig) (Y : pts_sig X) (FTM : f_term_mod X) (FEM : f_en
 
   Open Scope RR_scope.
 
-  Scheme typ_ind' := Induction for typ   Sort Prop
-                    with   wf_ind' := Induction for wf    Sort Prop
-                                     with typh_ind' := Induction for typ_h Sort Prop.
+  Scheme typ_ind'  := Induction for typ   Sort Prop
+  with   wf_ind'   := Induction for wf    Sort Prop
+  with   typh_ind' := Induction for typ_h Sort Prop.
 
-  Combined Scheme typ_induc from typ_ind', typh_ind',wf_ind'.
+  Combined Scheme typ_induc from typ_ind', typh_ind', wf_ind'.
 
   (** some simple rewrite rules, if the statement P is an conjunction*)
   Ltac rewrite_l P := rewrite ((and_ind (fun A _ => A)) P).
@@ -116,8 +122,10 @@ Module f_typ_mod (X : term_sig) (Y : pts_sig X) (FTM : f_term_mod X) (FEM : f_en
   Definition has_type A Γ := (exists B, Γ ⊢ A : B).
   Definition is_type  A Γ := (exists B, Γ ⊢ B : A).
   Definition typ_h_short Γ A B := (exists H, Γ ⊢ H : A = B).
-  Notation "Γ ⊢ M = N" := (typ_h_short Γ M N) (at level 80, M, N at level 30, no associativity).
-  Notation "Γ ⊢ A : B : C" := (Γ ⊢ A : B/\Γ ⊢ B : C) (at level 80, A, B, C at level 30, no associativity).
+  Notation "Γ ⊢ M = N" := (typ_h_short Γ M N)
+                         (at level 80, M, N at level 30, no associativity).
+  Notation "Γ ⊢ A : B : C" := (Γ ⊢ A : B/\Γ ⊢ B : C)
+                             (at level 80, A, B, C at level 30, no associativity).
 
   (** Basic properties of PTS.
   Context Validity: if a judgment is valid, its context is well-formed.*)
@@ -126,35 +134,51 @@ Module f_typ_mod (X : term_sig) (Y : pts_sig X) (FTM : f_term_mod X) (FEM : f_en
   Qed.
 
   Theorem weakening: 
-    (forall Γ   M N, Γ ⊢     M : N -> forall Δ A s n Γ', ins_in_env Δ A n Γ Γ' -> Δ ⊢ A : !s -> Γ' ⊢              M ↑ 1 # n : N ↑ 1 # n ) /\
-    (forall Γ H M N, Γ ⊢ H : M = N -> forall Δ A s n Γ', ins_in_env Δ A n Γ Γ' -> Δ ⊢ A : !s -> Γ' ⊢ H ↑h 1 # n : M ↑ 1 # n = N ↑ 1 # n ) /\
-    (forall Γ      , Γ ⊣           -> forall Δ A s n Γ', ins_in_env Δ A n Γ Γ' -> Δ ⊢ A : !s -> Γ' ⊣).
+    (forall Γ M N, Γ ⊢ M : N ->
+     forall Δ A s n Γ', ins_in_env Δ A n Γ Γ' -> Δ ⊢ A : !s ->
+       Γ' ⊢ M ↑ 1 # n : N ↑ 1 # n ) /\
+    (forall Γ H M N, Γ ⊢ H : M = N ->
+     forall Δ A s n Γ', ins_in_env Δ A n Γ Γ' -> Δ ⊢ A : !s ->
+       Γ' ⊢ H ↑h 1 # n : M ↑ 1 # n = N ↑ 1 # n ) /\
+    (forall Γ, Γ ⊣ -> forall Δ A s n Γ', ins_in_env Δ A n Γ Γ' -> Δ ⊢ A : !s -> Γ' ⊣).
   Admitted.
 
   Theorem thinning : forall Γ M N A s, Γ ⊢ M : N -> Γ ⊢ A : !s -> A::Γ ⊢ M ↑ 1 : N ↑ 1.
-    intros;eapply weakening;eassumption||econstructor.
+    intros ; eapply weakening ; eassumption || econstructor.
   Qed.
 
   (* First we define a transport that would come in handy. *)
 
-  Axiom transport : Sorts -> Term -> Term -> Term -> Term.
+  Open Scope F.
 
-  Axiom transport_typ :
-    forall Γ s t A A' p,
-      Ax s t ->
-      Rel t t t ->
-      Rel s s s ->
-      Rel t s t ->
-      Γ ⊢ A  : !s ->
-      Γ ⊢ A' : !s ->
-      Γ ⊢ p  : Id !s A A' ->
-      Γ ⊢ transport s A A' p : Π(A), A' ↑ 1.
+    (* Axiom transport : Sorts -> FTM.Term -> FTM.Term -> FTM.Term -> FTM.Term. *)
+
+    (* DAMN! Is it possible to talk about typing in the other system???!! *)
+    (* Axiom transport_typ : *)
+    (*   forall (Γ : FEM.Env) (s t : Sorts) (A A' p : FTM.Term), *)
+    (*     Ax s t -> *)
+    (*     Rel t t t -> *)
+    (*     Rel s s s -> *)
+    (*     Rel t s t -> *)
+    (*     (f_typ_mod X Y FTM FEM).typ Γ A !s -> *)
+    (*     Γ ⊢ A  : !s -> *)
+    (*     Γ ⊢ A' : !s -> *)
+    (*     Γ ⊢ p  : Id !s A A' -> *)
+    (*     Γ ⊢ transport s A A' p : Π(A), A' ↑ 1. *)
+
+    (* Maybe we can hope for a version that doesn't need the types. *)
+    Axiom transport : FTM.Term -> FTM.Term.
+    Notation "p ⋆" := (transport p) (at level 3).
+
+  Close Scope F.
 
   (* Let's start the translation to PTSf *)
 
   Reserved Notation "⦑ A ⦒τ" (at level 7, no associativity).
   Reserved Notation "⦑ H ⦒α" (at level 7, no associativity).
 
+  (* We need to have some information about the type if we want to *)
+  (* be able to make the transport or proofs such as Rfl... *)
   Fixpoint unrrt (t : Term) : FTM.Term :=
     match t with
     | #v            => (#v)%F
@@ -165,40 +189,48 @@ Module f_typ_mod (X : term_sig) (Y : pts_sig X) (FTM : f_term_mod X) (FEM : f_en
     | Id A u v      => FTM.Id ⦑A⦒τ ⦑u⦒τ ⦑v⦒τ
     | Rfl A u       => FTM.Rfl ⦑A⦒τ ⦑u⦒τ
     | J A C b u v p => FTM.J ⦑A⦒τ ⦑C⦒τ ⦑b⦒τ ⦑u⦒τ ⦑v⦒τ ⦑p⦒τ
-    | t ∽ H         => match unrrtp ⦑t⦒τ H with
-                      | (t,H) => (t ∽ H)%F
-                      end
+    | t ∽ H         => (⦑H⦒α ⋆ · ⦑t⦒τ)%F
     | RRAA          => RE.BB
     | Inj t         => (RE.ff · ⦑t⦒τ)%F
     | Proj t        => (RE.gg · ⦑t⦒τ)%F
     end
     where "⦑ A ⦒τ" := (unrrt A)
-  with unrrp (H : Prf) : FTM.Term * FTM.Prf :=
+  with unrrp (H : Prf) : FTM.Term :=
     match H with
-    | ρ A                           => (t, ρ ⦑A⦒τ)%F
-    | H †                           => match unrrtp t H with
-                                      | (t,H) => (t,H †)%F
-                                      end
-    | H1 • H2                       => match unrrtp t H1 with
-                                      | (t,H) => unrrtp (t ∽ H)%F H2
-                                      end
-    | β A                           => (t, β ⦑A⦒τ)%F
+    | ρ A => FTM.Rfl (#0)%F ⦑A⦒τ
+    (* | H †                           => match unrrtp t H with *)
+    (*                                   | (t,H) => (t,H †)%F *)
+    (*                                   end *)
+    (* | H1 • H2                       => match unrrtp t H1 with *)
+    (*                                   | (t,H) => unrrtp (t ∽ H)%F H2 *)
+    (*                                   end *)
+    (* | β A                           => (t, β ⦑A⦒τ)%F *)
     (* | { H1, [A] H2 } => ??? *)
     (* | ⟨ H1, [A] H2 ⟩  => ??? *)
     (* | H1 ·h H2       => ??? *)
     (* Maybe in the three cases above we should do the translation with Refl instead of each Hi *)
     (* | ι (a ∽ H)       => ??? *)
-    | ι A                           => (#0, FTM.Refl #0)%F (* This shouldn't happen so... wildcard *)
+    (* | ι A                           => (#0, FTM.Refl #0)%F *) (* This shouldn't happen so... wildcard *)
     (* | IdEq HA Hu Hv  => ??? *)
     (* | RflEq HA Hu    => ??? *)
     (* | JEq HA HC Hb Hu Hv Hp => ??? *)
-    | JRed (J A C b u v (Rfl B w)) => (t, FTM.JRed (FTM.J ⦑A⦒τ ⦑C⦒τ ⦑b⦒τ ⦑u⦒τ ⦑v⦒τ (FTM.Rfl ⦑B⦒τ ⦑w⦒τ)))
+    (* | JRed (J A C b u v (Rfl B w)) => (t, FTM.JRed (FTM.J ⦑A⦒τ ⦑C⦒τ ⦑b⦒τ ⦑u⦒τ ⦑v⦒τ (FTM.Rfl ⦑B⦒τ ⦑w⦒τ))) *)
     (* | PI a                         =>  *)
-    | H                            => (#0, FTM.Refl #0)%F
+    | H => (#0)%F
     end
-    where "⦑ H ⦒α" := (unrrp A)
+    where "⦑ H ⦒α" := (unrrp H).
 
   Notation "⦑ A ⦒τ" := (unrrt A) (at level 7, no associativity).
+  Notation "⦑ H ⦒α" := (unrrp H) (at level 7, no associativity).
+
+  Definition unrrenv (Γ : Env) : FEM.Env :=
+    map unrrt Γ.
+  Notation "⦑ Γ ⦒γ" := (unrrenv Γ) (at level 7, no associativity).
+
+  Theorem trans_compat :
+    (forall Γ t T, Γ ⊢ t : T -> (⦑Γ⦒γ ⊢ ⦑t⦒τ : ⦑T⦒τ)%F) /\
+    (forall Γ H A B, Γ ⊢ H : A = B -> exists s, (⦑Γ⦒γ ⊢ ⦑H⦒α : FTM.Id !s ⦑A⦒τ ⦑B⦒τ)%F).
+
 
 End f_typ_mod.
 
