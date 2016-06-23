535045843f14f6fcad0f111cbe4599e88d0b433d
Théo Winterhalter <isheeft@gmail.com>
2016-06-23 15:45:56 +0200

    Complete Prf

--- formol/XTerms.v
+++ formol/XTerms.v
@@ -16,11 +16,18 @@ Inductive Term :=
 | Proj : Term  -> Term
 | Conv : Term  -> Prf  -> Term
 with Prf :=
-| Refl  : Term -> Prf
-| Sym   : Prf  -> Prf
-| Trans : Prf  -> Prf  -> Prf
-| Beta  : Term -> Term -> Term -> Prf
-(* | What's next? *)
+| Refl   : Term -> Prf
+| Sym    : Prf  -> Prf
+| Trans  : Prf  -> Prf  -> Prf
+| Beta   : Term -> Term -> Term -> Prf
+| ProdEq : Prf  -> Term -> Prf  -> Prf
+| AbsEq  : Prf  -> Term -> Prf  -> Prf
+| AppEq  : Prf  -> Prf  -> Prf
+| IdEq   : Prf  -> Prf  -> Prf  -> Prf
+| RflEq  : Prf  -> Prf  -> Prf
+| JEq    : Prf  -> Prf  -> Prf  -> Prf  -> Prf -> Prf -> Prf
+| JRed   : Term -> Term -> Term -> Term -> Prf
+| ConvEq : Prf  -> Prf  -> Prf
 .
 
 Notation "! s" := (Sort s) (at level 1) : X_scope.
@@ -30,4 +37,16 @@ Notation "'Π' ( U ) , V" := (Prod U V)
 Notation "'λ' [ U ] , v" := (Abs U v)
                            (at level 20, U , v at level 30) : X_scope.
 Notation "x · y" := (App x y) (at level 15, left associativity) : X_scope.
+Notation "t ∽ H" := (Conv t H) (at level 15) : X_scope.
+Notation "'ρ' A" := (Refl A) (at level 6) : X_scope. 
+Notation "H †" := (Sym H) (at level 6) : X_scope.
+Notation "H1 • H2" := (Trans H1 H2) (at level 15, left associativity) : X_scope.
+Notation "'β' A b a" := (Beta A b a) (at level 6) : X_scope.
+Notation "{ H1 , [ A ] H2 }" := (ProdEq H1 A H2) (at level 15) : X_scope.
+Notation "⟨ H1 , [ A ] H2 ⟩" := (AbsEq H1 A H2)
+                              (at level 15, left associativity) : X_scope.
+Notation "H1 ·h H2" := (AppEq H1 H2)
+                      (at level 15, left associativity) : X_scope.
+Notation "H1 ∽h H2" := (ConvEq H1 H2) (at level 15) : X_scope.
+
 
