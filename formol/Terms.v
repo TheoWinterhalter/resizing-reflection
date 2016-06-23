677b20de0672a6a16a3565c55c4fbd05b0a4640c
Théo Winterhalter <isheeft@gmail.com>
2016-06-23 16:08:24 +0200

    Start Typing

--- formol/Terms.v
+++ formol/Terms.v
@@ -21,3 +21,5 @@ Notation "'λ' [ U ] , v" := (Abs U v)
                            (at level 20, U , v at level 30) : T_scope.
 Notation "x · y" := (App x y) (at level 15, left associativity) : T_scope.
 
+Definition Env := list Term.
+
